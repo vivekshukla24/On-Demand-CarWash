477cf8e9059232216f862c8ab5be8f48478671bb
vivekshukla24 <vivek-kumar.shukla@capgemini.com>
2021-12-07 15:23:16 +0530

    update

--- order/src/main/java/CG/order/OrderApplication.java
+++ order/src/main/java/CG/order/OrderApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderApplication {
+public class OrderApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
