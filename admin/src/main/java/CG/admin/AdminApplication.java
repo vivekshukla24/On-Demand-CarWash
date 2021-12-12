72b33a84b6da78a8d053a3f5989266d09fc354f7
vivekshukla24 <versesshukla123@gmail.com>
2021-12-12 23:26:37 +0530

    added swagger with UI and added a swaggerConfig, updates to pom.xml.

--- admin/src/main/java/CG/admin/AdminApplication.java
+++ admin/src/main/java/CG/admin/AdminApplication.java
@@ -11,8 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
 public class AdminApplication implements CommandLineRunner {
 	@Autowired
@@ -25,6 +34,28 @@ public class AdminApplication implements CommandLineRunner {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
+	}
+	@Bean
+	public Docket SwaggerConfig(){
+		//Returns a prepared docket instance
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/admins/**"))
+				.apis(RequestHandlerSelectors.basePackage("CG.admin"))
+				.build()
+				.apiInfo(apiinform());
+
+	}
+	private ApiInfo apiinform(){
+		return new ApiInfo(
+				"Admin's Panel",
+				"The Admin has all the below controls",
+				"1.0",
+				"Can be used by anyone testing the app",
+				new springfox.documentation.service.Contact("Vivek Shukla","https://github.com/vivekshukla24","vivek@gmail.com"),
+				"API license",
+				"https://github.com/vivekshukla24",
+				Collections.emptyList());
 	}
 
 	@Override
--- washer/src/main/java/CG/washer/WasherApplication.java
+++ washer/src/main/java/CG/washer/WasherApplication.java
@@ -9,8 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
 public class WasherApplication implements CommandLineRunner {
 	@Bean
@@ -21,6 +30,29 @@ public class WasherApplication implements CommandLineRunner {
 	private WasherRepository wr;
 	public static void main(String[] args) {
 		SpringApplication.run(WasherApplication.class, args);
+	}
+
+	@Bean
+	public Docket SwaggerConfig(){
+		//Returns a prepared docket instance
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/washers/**"))
+				.apis(RequestHandlerSelectors.basePackage("CG.washer"))
+				.build()
+				.apiInfo(apiinform());
+
+	}
+	private ApiInfo apiinform(){
+		return new ApiInfo(
+				"Washer's Panel",
+				"The Washer has all the below controls",
+				"1.0",
+				"Can be used by anyone testing the app",
+				new springfox.documentation.service.Contact("Vivek Shukla","https://github.com/vivekshukla24","vivek@gmail.com"),
+				"API license",
+				"https://github.com/vivekshukla24",
+				Collections.emptyList());
 	}
 
 	@Override
