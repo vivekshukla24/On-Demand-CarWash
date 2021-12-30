2dc01b4fa15eafa16a9144fd5e9c0833c316f1ed
vivekshukla24 <versesshukla123@gmail.com>
2021-12-31 00:46:24 +0530

    final changes to MS and pom

--- washer/src/main/java/CG/washer/model/OrderDetails.java
+++ washer/src/main/java/CG/washer/model/OrderDetails.java
@@ -2,7 +2,6 @@ package CG.washer.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
