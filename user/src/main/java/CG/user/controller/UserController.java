21b71fd6e6dd7316788f0023411c2629d304952b
vivekshukla24 <versesshukla123@gmail.com>
2022-01-05 22:55:07 +0530

    adding arch. for project

--- user/src/main/java/CG/user/controller/UserController.java
+++ user/src/main/java/CG/user/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/users")
 public class UserController {
